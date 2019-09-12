2d533165dbfde9996620006e11d8c6c4de5aa213
John Hencken <jhenck57@scarletmail.rutgers.edu>
2019-09-11 20:45:56 -0400

    minor

--- launch_service_jupyterhub.sh
+++ launch_service_jupyterhub.sh
@@ -1,9 +1,9 @@
 docker service create \
   --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   --mount type=bind,src=/etc/jupyterhub,dst=/srv/jupyterhub \
-  #--mount src=nfsvolume,dst=/var/nfs \
   --name jupyterhubserver \
   --network jupyterhub \
   --constraint 'node.role == manager' \
   --detach=true \
   jhenck57/arc_gis:jupyterhub-docker
+ #--mount src=nfsvolume,dst=/var/nfs \
