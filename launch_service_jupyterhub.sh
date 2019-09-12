d65ad19a0ed87a66c5109ca0cf592ec62a99e69c
John Hencken <jhenck57@scarletmail.rutgers.edu>
2019-09-11 22:31:06 -0400

    adding .local

--- launch_service_jupyterhub.sh
+++ launch_service_jupyterhub.sh
@@ -1,7 +1,7 @@
 docker service create \
   --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   --mount type=bind,src=/hosthome/administrator/Documents/docker/templates/docker-jupyterhub-nginx/deploy-jupyterhub-dockerswarm,dst=/srv/jupyterhub \
-  --name jupyterhubserver \
+  --name jupyterhubserver.local \
   --network jupyterhub \
   --constraint 'node.role == manager' \
   --detach=true \
