b8ec681107ea826c1a38e1393a2fccf81aeec2ab
John Hencken <jhenck57@scarletmail.rutgers.edu>
2019-09-11 20:42:42 -0400

    following along

--- launch_service_jupyterhub.sh
+++ launch_service_jupyterhub.sh
@@ -1,7 +1,7 @@
 docker service create \
   --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   --mount type=bind,src=/etc/jupyterhub,dst=/srv/jupyterhub \
-  --mount src=nfsvolume,dst=/var/nfs \
+  #--mount src=nfsvolume,dst=/var/nfs \
   --name jupyterhubserver \
   --network jupyterhub \
   --constraint 'node.role == manager' \
